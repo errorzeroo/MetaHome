b89625271c7936943ee1252d2f5f9c67f7f096da
rytjr <kks007159@naver.com>
2025-01-13 15:40:43 +0900

    add Intellij project: spring-mybatis2

---
+++ spring-mybatis2/src/test/java/com/metafinal/spring_mybatis2/SpringMybatis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metafinal.spring_mybatis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatis2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
