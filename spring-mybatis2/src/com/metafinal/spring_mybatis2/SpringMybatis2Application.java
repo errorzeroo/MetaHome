33935686e7464bd517b692e1873c7b64ac7e6865
sejin <dlatpwls524@gmail.com>
2025-01-13 17:30:56 +0900

    applicaiton properties add

--- spring-mybatis2/src/com/metafinal/spring_mybatis2/SpringMybatis2Application.java
+++ spring-mybatis2/src/com/metafinal/spring_mybatis2/SpringMybatis2Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMybatis2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMybatis2Application.class, args);
+		System.out.println("test");
 	}
 
 }
