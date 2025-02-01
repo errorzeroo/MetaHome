e14eb85da69ffe0ee5d6fc3f533ef69a11bc8635
errorzerroo <dlatpwls524@gmail.com>
2025-02-01 17:14:28 +0900

    불필요한 파일 제거

--- spring-mybatis2/src/main/resources/static/js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("slideToggleButton").addEventListener("click", function () {
-            const panel = document.getElementById("slidingPanel");
-            panel.classList.toggle("active");
-
-            // 버튼 아이콘 변경 (❮ ↔ ❯)
-            this.textContent = panel.classList.contains("active") ? "❮" : "❯";
-        });
